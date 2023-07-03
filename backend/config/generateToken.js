f5794b18f60a509e6012818b4993e5f32a99b0ca
KeyurModi5 <105266106+KeyurModi5@users.noreply.github.com>
2023-07-03 14:42:37 +0530

    Update generateToken.js

--- backend/config/generateToken.js
+++ backend/config/generateToken.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const generateToken = (id) => {
-  return jwt.sign({ id }, "keyurmodi", { expiresIn: "30d" });
+  return jwt.sign({ id }, "", { expiresIn: "30d" });
 };
 
 module.exports = generateToken;
